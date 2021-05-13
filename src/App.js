96b0e3d84c5da1e3727a6f105906a96e43df72b3
Celig <cela@protonmail.com>
2021-05-13 19:10:18 +0200

    Add heading

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import './App.sass';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
+
+      <div className="heading">
+        <h1 className="title">Color Contrast Checker</h1>
+        <p className="subtitle">Calculate the contrast ratio of text and background colors</p>
+      </div>
+
     </div>
   );
 }
